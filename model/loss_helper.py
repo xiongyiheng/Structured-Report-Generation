06daeca82eff5aa9ffb63cc1650c398350a00c09
xiongyiheng <763285416@qq.com>
2022-07-02 16:15:06 +0200

    latest training setting - 2

--- model/loss_helper.py
+++ model/loss_helper.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 def get_loss(pre_label, gt_label):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    POS_WEIGHT = torch.ones(1215) * 3.0
+    POS_WEIGHT = torch.ones(899) * 3.0
     POS_WEIGHT = POS_WEIGHT.to(device)
     criterion = nn.BCEWithLogitsLoss(pos_weight=POS_WEIGHT)
     loss = criterion(pre_label, gt_label)
